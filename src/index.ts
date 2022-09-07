318d7bf80adc964435fbaffd2034f3ec025da8ff
joseph <joseph.ketterer@gmail.com>
2022-09-07 12:49:39 +0100

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Dog from "./Dog.js";
-import ShelterDog from "./ShelterDog.js";
-import { add, multiply, divide } from "./utils.js";
-console.log("FROM INDEX FILE!");
+import Dog from "./Dog";
+import ShelterDog from "./ShelterDog";
+import { add, multiply, divide } from "./utils";
+console.log("FROM INDEX FILE!!!!!!!!");
 
 const elton = new Dog("Elton", "Aussie", 0.5);
 elton.bark();
